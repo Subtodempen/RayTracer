7629135a0ae683e3da541db3aaa1dab63e11dea1
subtodempen <pehlivandemirel@yahoo.com>
2025-10-11 17:02:47 +0100

    Started Basic Camera Code

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,16 +12,37 @@
 #define RGBMAX 255
 
 
-struct RGB{
+typedef struct {
     uint8_t r, b, g;
-};
+}RGB;
+
+typedef struct{
+    int x, y, z;
+}vec3;
+
+
+typedef struct{
+    vec3 origin;
+    vec3 direction; 
+}ray;
+
+vec3 calcRayPos(ray r, int t);
+
+// vector functionailty
+static inline vec3 add(vec3 a, vec3 b);
+static inline vec3 scalerMultiply(vec3 a, int n);
+static inline vec3 subtract(vec3 a, vec3 b);
+
+static inline int magnitude(vec3 a);
+static inline int dotProduct(vec3 a, vec3 b);
 
 // read and write to the ppm file and functionbailty
 //int readPPM(FILE* f);
-void writePPMPixels(FILE* f, struct RGB* pixels, const int size);
+void writePPMPixels(FILE* f, RGB* pixels, const int size);
 void initPPMHeader(FILE* f, int n, int m);
 
 int openPPMFile(FILE** f, const char* fname);
+
 
 int openPPMFile(FILE** f, const char* fname){
     *f = fopen(fname, "w");
@@ -41,12 +63,83 @@ void initPPMHeader(FILE* f, int n, int m){
     free(str);
 }
 
-void writePPMPixels(FILE* f, struct RGB* pixels, const int size){
-    fwrite(pixels, size / sizeof(struct RGB), size, f);
+void writePPMPixels(FILE* f, RGB* pixels, const int size){
+    fwrite(pixels, size / sizeof(RGB), size, f);
 }
 
+// vec3 functionailty dit product adding etc etc
+vec3 add(vec3 a, vec3 b){
+    vec3 product;
+
+    product.x = a.x + b.x;
+    product.y = a.y + b.y;
+    product.z = a.z + b.z;
+
+    return product;
+}
+
+vec3 scalerMultiply(vec3 a, int n){
+    vec3 product;
+
+    product.x = a.x * n;
+    product.y = a.y * n;
+    product.z = a.z * n;
+
+    return product;
+}
+
+vec3 subtract(vec3 a, vec3 b){
+    b = scalerMultiply(b, -1);
+    return add(a, b);
+}
+
+vec3 divide(vec3 a, int n){
+    scalerMultiply(a, 1/n);
+}
+
+int magnitude(vec3 a){
+    return sqrt((a.x * a.x) + (a.y * a.y) + (a.z * a.z));
+}
+
+int dotProduct(vec3 a, vec3 b){
+    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+}
+
+vec3 calcUnitVector(vec3 a){
+    return divide(a, magnitude(a));
+}
+
+vec3 calcRayPos(ray r, int t){
+    return add(scalerMultiply(r.direction, t), r.origin);
+}
 
 int main(){
+    double aspectRatio = 16.0 / 9.0;
+    
+    int imageWidth = XSIZE;
+    int imageHeight = imageWidth / (int)aspectRatio;
+
+    double viewportWidth = 2.0;
+    double viewportHeight = viewportWidth / aspectRatio;
+
+    double deltaU = imageWidth / viewportWidth;
+    double deltaV = imageHeight / viewportHeight;
+
+    int focalPoint = 1;
+
+    vec3 cameraOrigin = {0, 0, 0};
+
+    for(int i=0; i < imageWidth; i++){
+        for(int j=0; j < imageHeight; j++){
+            vec3 pixelCentre = {i * deltaU, j * deltaV, focalPoint};
+            vec3 rayDirection = calcUnitVector(pixelCentre);
+
+            ray r = {cameraOrigin, rayDirection};
+
+        }
+    }
+}
+    /*
     const int size = XSIZE * YSIZE;
     
     struct RGB* pixels[YSIZE];
@@ -81,3 +174,5 @@ int main(){
     free(pixels[0]);
     fclose(f);
 }
+
+*/
