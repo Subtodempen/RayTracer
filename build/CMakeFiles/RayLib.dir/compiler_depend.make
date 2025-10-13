bf0422e4a0f6116a9e6830a67e7ee529b1ae4f6c
subtodempen <pehlivandemirel@yahoo.com>
2025-10-13 22:13:52 +0100

    Rebuilding test

---
+++ build/CMakeFiles/RayLib.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for RayLib.
+# This may be replaced when dependencies are built.
--- tests/test.py
+++
@@ -1,5 +0,0 @@
-from _testModule import ffi
-lib = ffi.dlopen("../build/libRayLib.so")
-
-v = ffi.new("vec3*", {'x': 2, 'y': 2, 'z': 2})
-print(lib.magnitude(v[0]))
